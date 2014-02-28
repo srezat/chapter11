4fc02102d994fc873ebeff6c91dcf0a89173df24
me <me@me-rubyonrails.(none)>
2014-02-28 22:12:03 +0330

    Add a StaticPages controller

---
+++ 4.0.3-upgrade/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'ae29253db915d5fc9202f14cf23c288e7e558ec2fc2638f6cb9f535250834cbd9d9c202916014805a70810a2e45f9a5562d6efabdf00eaf37f492033e9274630';
