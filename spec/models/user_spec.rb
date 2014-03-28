2c35ca2f74113f98f987b683c3e559b204b440b9
me <me@me-rubyonrails.(none)>
2014-03-29 03:51:43 +0430

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+	@user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -12,7 +13,7 @@ describe User do
     it { should respond_to(:name) }
     it { should respond_to(:email) } 
     it { should respond_to(:password_digest) }
-
+    it { should respond_to(:remember_token) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
 
@@ -28,6 +29,10 @@ describe User do
     it { should_not be_valid }
   end
 
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
  describe "when name is not present" do
     before { @user.name = " " }
