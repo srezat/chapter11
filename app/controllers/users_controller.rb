dbd5e27b347f97d155756a320b4b11e3d59aa71b
me <me@me-rubyonrails.(none)>
2014-04-04 20:48:51 +0430

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -8,17 +16,36 @@ class UsersController < ApplicationController
 	@user = User.new
 
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
 
-# def create
- #   @user = User.new(params[:user])    # Not the final implementation!
-#    if @user.save
-#      # Handle a successful save.
+
+  def edit
+#    @user = User.find(params[:id])
+  end
+
+
+#def update
+#    @user = User.find(params[:id])
+#    if @user.update_attributes(user_params)
+#      flash[:success] = "Profile updated"
+#      redirect_to @user
 #    else
-#      render 'new'
+#      render 'edit'
 #    end
 #  end
 
-
+ def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
 def create
     @user = User.new(user_params)
@@ -34,10 +61,26 @@ def create
 
   private
 
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
 
+  # Before filters
+    def signed_in_user
+	unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+        end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ describe User do
   subject { @user }
 
     it { should respond_to(:authenticate) }
+    it { should respond_to(:admin) }
     it { should respond_to(:name) }
     it { should respond_to(:email) } 
     it { should respond_to(:password_digest) }
@@ -18,7 +19,16 @@ describe User do
     it { should respond_to(:password_confirmation) }
 
     it { should be_valid }
+    it { should_not be_admin }
 
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
